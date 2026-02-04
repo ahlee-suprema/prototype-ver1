037fc9841bfab6f5b91301593c1a40ee6cd9499d
ahlee-suprema <ahlee@suprema.co.kr>
2026-02-04 14:34:14 +0900

    외근/출장 추가

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zZSfyaZYEcFQCcyb4iTLEY7uidMwH75RkOFQn408Uec=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
