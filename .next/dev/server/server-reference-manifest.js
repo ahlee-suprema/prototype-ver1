075c9794fe74192c4c98129698f6f1f047a7cd56
ahlee-suprema <ahlee@suprema.co.kr>
2026-02-04 12:55:44 +0900

    ver.1

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zZSfyaZYEcFQCcyb4iTLEY7uidMwH75RkOFQn408Uec=\"\n}"
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Suprema T&A!</h1>
+    </div>
+  );
+}
